d456c16a529e45a0525009af8ffb86d8b03874bf
Tommy Petersen <tp@aia.dk>
2025-11-28 20:32:28 +0100

    Omdannet tabulator til mellemrum.

--- IldgrubenSpil/Laboratorie/oevelser_afsnit05.asm
+++ IldgrubenSpil/Laboratorie/oevelser_afsnit05.asm
@@ -1,43 +1,43 @@
-	BasicUpstart2(start)
+        BasicUpstart2(start)
 
 start:
-	lda #8		// H
-	sta $0400
+        lda #8          // H
+        sta $0400
 
-	lda #5		// E
-	sta $0401
+        lda #5          // E
+        sta $0401
 
-	lda #10		// J
-	sta $0402
+        lda #10         // J
+        sta $0402
 
-	lda #32		// MELLEMRUM
-	sta $0403
+        lda #32         // MELLEMRUM
+        sta $0403
 
-	lda #3		// C
-	sta $0404
+        lda #3          // C
+        sta $0404
 
-	lda #54		// 6
-	sta $0405
+        lda #54         // 6
+        sta $0405
 
-	lda #52		// 4
-	sta $0406
-	
-	lda #32		// MELLEMRUM
-	sta $0407
+        lda #52         // 4
+        sta $0406
+        
+        lda #32         // MELLEMRUM
+        sta $0407
 
-	lda #$F2	// HEXADECIMAL
-	sta $0408
+        lda #$F2        // HEXADECIMAL
+        sta $0408
 
-	lda #32		// MELLEMRUM
-	sta $0409
+        lda #32         // MELLEMRUM
+        sta $0409
 
-	lda #%00100110	// BINAER
-	sta $040A
+        lda #%00100110  // BINAER
+        sta $040A
 
-	lda #32		// MELLEMRUM
-	sta $040B
+        lda #32         // MELLEMRUM
+        sta $040B
 
-	lda $A004	// LAGERADRESSE
-	sta $040C
+        lda $A004       // LAGERADRESSE
+        sta $040C
 
-	rts
+        rts
--- IldgrubenSpil/Laboratorie/oevelser_afsnit11.asm
+++ IldgrubenSpil/Laboratorie/oevelser_afsnit11.asm
@@ -1,90 +1,90 @@
-	BasicUpstart2(kontrollogik)
+        BasicUpstart2(kontrollogik)
 
-	// *******************************
-	// *** H O V E D P R O G R A M ***
-	// *******************************
+        // *******************************
+        // *** H O V E D P R O G R A M ***
+        // *******************************
 kontrollogik:
-	lda #%00000000
-	ora #%00000001		// 00000001
-	lsr			// 00000000, mente: 1
-	adc #0			// 00000001
-	ora #%10000000		// 10000001
-	asl			// 00000010, mente: 1
-	adc #0			// 00000011
-	and #%00000011		// 00000011
-	cmp #%00000011
-	bne deErIkkeEns
+        lda #%00000000
+        ora #%00000001          // 00000001
+        lsr                     // 00000000, mente: 1
+        adc #0                  // 00000001
+        ora #%10000000          // 10000001
+        asl                     // 00000010, mente: 1
+        adc #0                  // 00000011
+        and #%00000011          // 00000011
+        cmp #%00000011
+        bne deErIkkeEns
 deErEns:
-	jsr begyndIndlaes1
-	jmp hovedloekke
-deErIkkeEns:	
-	jsr begyndIndlaes2
+        jsr begyndIndlaes1
+        jmp hovedloekke
+deErIkkeEns:
+        jsr begyndIndlaes2
 hovedloekke:
-	jsr kopierKantfarve
-	jmp hovedloekke
+        jsr kopierKantfarve
+        jmp hovedloekke
 
-	// ***********************************
-	// *** H J A E L P E R U T I N E R ***
-	// ***********************************
+        // ***********************************
+        // *** H J A E L P E R U T I N E R ***
+        // ***********************************
 begyndIndlaes1:
-	ldx #0	//x := 0
+        ldx #0  //x := 0
 loekke1: //Indlaes fra stamtabel til arbejdstabel
-	lda stamtabel1,x
-	sta arbejdstabel,x
-	inx
-	cpx #9
-	bne loekke1
-	rts
+        lda stamtabel1,x
+        sta arbejdstabel,x
+        inx
+        cpx #9
+        bne loekke1
+        rts
 begyndIndlaes2:
-	ldx #0	//x := 0
+        ldx #0  //x := 0
 loekke2: //Indlaes fra stamtabel til arbejdstabel
-	lda stamtabel2,x
-	sta arbejdstabel,x
-	inx
-	cpx #9
-	bne loekke2
-	rts
+        lda stamtabel2,x
+        sta arbejdstabel,x
+        inx
+        cpx #9
+        bne loekke2
+        rts
 
 kopierKantfarve:
-	ldy #0	//y := 0
+        ldy #0  //y := 0
 kopier: //Kopiér fra arbejdstabel til kantfarve
-	lda arbejdstabel,y
-	sta $d020
-	iny
-	cpy #9
-	bne kopier
-	rts
-	
-	// ***************
-	// *** D A T A ***
-	// ***************
+        lda arbejdstabel,y
+        sta $d020
+        iny
+        cpy #9
+        bne kopier
+        rts
+
+        // ***************
+        // *** D A T A ***
+        // ***************
 arbejdstabel:
-	.byte 0
-	.byte 0
-	.byte 0
-	.byte 0
-	.byte 0
-	.byte 0
-	.byte 0
-	.byte 0
-	.byte 0
+        .byte 0
+        .byte 0
+        .byte 0
+        .byte 0
+        .byte 0
+        .byte 0
+        .byte 0
+        .byte 0
+        .byte 0
 stamtabel1:
-	.byte 1
-	.byte 1
-	.byte 1
-	.byte 1
-	.byte 1
-	.byte 1
-	.byte 1
-	.byte 1
-	.byte 1
+        .byte 1
+        .byte 1
+        .byte 1
+        .byte 1
+        .byte 1
+        .byte 1
+        .byte 1
+        .byte 1
+        .byte 1
 stamtabel2:
-	.byte 0
-	.byte 1
-	.byte 2
-	.byte 3
-	.byte 4
-	.byte 5
-	.byte 6
-	.byte 7
-	.byte 8
+        .byte 0
+        .byte 1
+        .byte 2
+        .byte 3
+        .byte 4
+        .byte 5
+        .byte 6
+        .byte 7
+        .byte 8
